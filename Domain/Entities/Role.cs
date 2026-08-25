ab41dd495c8d81332c01c60238dc630a3c4e951e
DarkusA <ramiperez06austral@gmail.com>
2026-08-25 07:51:20 -0300

    Entities

---
+++ Domain/Entities/Role.cs
@@ -0,0 +1,10 @@
+namespace Domain.Entities;
+
+//Esto es un enum, no entidad pero no sabia si ponerlo con Entidadades o crear una carpeta de enums.
+public enum Role
+{
+    User,
+    OrderManager,
+    Admin,
+    SysAdmin
+}
---
+++ Domain/Entities/User.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+namespace Domain.Entities;
+
+public class User
+{
+    public int Id { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+    public Role Role { get; set; }
+}   
